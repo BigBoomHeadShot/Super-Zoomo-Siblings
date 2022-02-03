feea513102f77f0badacfedc3832f8b51a076db1
PrincessLulu <98086002+PrincessLulu@users.noreply.github.com>
2022-02-03 09:16:25 +0100

    made the movement thing and fixed the dash scriptso it works again

---
+++ Super Zoomo Silblings/Assets/Scripts/ActualMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActualMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical")), ForceMode2D.Force);
+    }
+}
