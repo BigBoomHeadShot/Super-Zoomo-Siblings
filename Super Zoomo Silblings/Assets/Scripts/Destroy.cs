7643a7e4cbf353cd99ea2b4677ec71f956060ed9
PrincessLulu <98086002+PrincessLulu@users.noreply.github.com>
2022-02-03 09:42:57 +0100

    changed the destroy script so players can die more

--- Super Zoomo Silblings/Assets/Scripts/Destroy.cs
+++ Super Zoomo Silblings/Assets/Scripts/Destroy.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour
 {
+    [SerializeField] private GameObject player1;
+    [SerializeField] private GameObject player2;
+    [SerializeField] private int player1Life = 3;
+    [SerializeField] private int player2Life = 3;
+    [SerializeField] private GameObject spawnPoint;
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        Destroy(collision.gameObject);
+        if (collision.gameObject == player1)
+        {
+            player1.SetActive(false);
+            player1Life -= 1;
+            player1.transform.position = new Vector2(spawnPoint.transform.position.x, spawnPoint.transform.position.y);
+            player1.SetActive(true);
+        }
+        if (collision.gameObject == player2)
+        {
+            player2.SetActive(false);
+            player2Life -= 1;
+            player2.transform.position = new Vector2(spawnPoint.transform.position.x, spawnPoint.transform.position.y);
+            player2.SetActive(true);
+        }
     }
 }
